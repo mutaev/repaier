ba06e36a2e4af775275123fe20cfde79692c10f3
eldar mutaev <eldarmutaev@inbox.ru>
2022-12-24 21:40:19 +0300

    почти закончил пк версию

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,37 @@
 $(function () { 
+
+
+  $('.reviews__slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 1111,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 765,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
+
+
+
   $("#phone").mask("+7 (999) 999-99-99");
-  // $("#phone-popup").mask("+7 (999) 999-99-99");
-  // $("#phone-down").mask("+7 (999) 999-99-99");
 })
