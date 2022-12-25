59668b5819c9cf76610d89d91d1e0e19b520fe18
eldar mutaev <eldarmutaev@inbox.ru>
2022-12-25 15:03:34 +0300

    готово

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,6 +13,12 @@ $(function () {
     nextArrow: $('.next'),
     responsive: [
       {
+        breakpoint: 1616,
+        settings: {
+          arrows: false,
+        }
+      },
+      {
         breakpoint: 1111,
         settings: {
           slidesToShow: 2,
@@ -30,8 +36,14 @@ $(function () {
     ]
   });
 
-
+  $('.menu-burger__header').click(function(){
+    $('.menu-burger__header').toggleClass('open-menu');
+    $('.header__nav').toggleClass('open-menu');
+    $('body').toggleClass('fixed-page');
+  });
 
 
   $("#phone").mask("+7 (999) 999-99-99");
+  $("#phone2").mask("+7 (999) 999-99-99");
+  $("#phone3").mask("+7 (999) 999-99-99");
 })
